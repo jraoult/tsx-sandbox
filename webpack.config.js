a13cf7f6f27f5879ef17d5d95092665cb4a755b4
Jonathan Raoult <jesuisjonathan@gmx.fr>
2019-04-25 14:53:24 +0100

    Initial sandbox

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin()]
+};
