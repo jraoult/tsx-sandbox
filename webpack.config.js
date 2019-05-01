f1d0c4f67e277b67f9bd6c373bf2b3d3bb5a6860
Jonathan Raoult <jesuisjonathan@gmx.fr>
2019-05-01 15:07:28 +0100

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
   },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
   module: {
     rules: [
       {
